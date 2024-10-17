c36880818bb9181cf1f1c36df79f1e489d552e7f
Michael Lourenco <michael.lourenco@pixter.com.br>
2024-10-17 20:13:50 -0300

    desafio dio java

---
+++ ContaBanco/README.md
@@ -0,0 +1,4 @@
+# JAVA BÁSICO
+Intrutor: Gleyson Sampaio
+
+Execução do desafio: [DIO.me JAVA](https://github.com/digitalinnovationone/trilha-java-basico/tree/main/desafios/sintaxe)
---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.next();  // Captura a agência como texto
+
+        scanner.nextLine(); 
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo inicial:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
+    }
+}
